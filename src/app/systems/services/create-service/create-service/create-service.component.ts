d7a0a91a686240f2895fbbec01c0cc139587ea55
Audrey Karera <u19206829@tuks.co.za>
2021-08-23 09:15:55 +0200

    service comp confirmation

--- src/app/systems/services/create-service/create-service/create-service.component.ts
+++ src/app/systems/services/create-service/create-service/create-service.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { SharedComponent } from 'src/app/component/shared components/shared/shared.component';
+import { DialogInterface } from 'src/app/interfaces/dialog.interface';
 
 interface service{
   value: string;
@@ -25,10 +27,43 @@ export class CreateServiceComponent implements OnInit {
     {value: 'collection-2', viewValue: 'Removal'}, 
   ];
   formGroup!: FormGroup;
-  constructor(public dialog: MatDialog
-    ) { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+  openConfirmDialog() {
+    const dialogInterface: DialogInterface = {
+      dialogHeader: 'Confirmation Message',
+      dialogContent: 'Are you sure you want to save this?',
+      cancelButtonLabel: 'No',
+      confirmButtonLabel: 'Yes',
+      callbackMethod: () => {
+       
+      },
+    };
+    this.dialog.open(SharedComponent, {
+      width: '300px',
+      data: dialogInterface,
+    });
+  }
+  
+  /**
+     * This method invokes the Cancel Dialog
+     */
+  openCancelDialog() {
+    const dialogInterface: DialogInterface = {
+      dialogHeader: 'Confirmation Message',
+      dialogContent: 'Are you sure you want cancel this ?',
+      cancelButtonLabel: 'No',
+      confirmButtonLabel: 'Yes',
+      callbackMethod: () => {
+       
+      },
+    };
+    this.dialog.open(SharedComponent, {
+      width: '300px',
+      data: dialogInterface,
+    });
   }
 
 }
