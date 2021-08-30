0f5f5a4211503e8b98c35e1f3e03c6f32a6fff3c
Tristan12-24 <62015939+Tristan12-24@users.noreply.github.com>
2021-08-30 11:55:27 +0200

    Idk
    I did not make changes here but i have to commit to move on

--- src/app/models/employmentStatus.ts
+++ src/app/models/employmentStatus.ts
@@ -1,4 +1,4 @@
-export class EmploymentStatus{
-    EmploymentStatusId: number;
-    EmploymentStatusName: string;
+export class EmploymentStatus {
+    EmploymentStatusId: number;  
+    EmploymentStatusName: string; 
 }
