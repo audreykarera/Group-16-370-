2f00963414464e04dbc1ce4300a01174ba1a1d10
Manesha99 <62016274+Manesha99@users.noreply.github.com>
2021-09-10 15:50:46 +0200

    Client - My bookings

--- src/app/client-side/read-client-bookings/read-client-booking/read-client-booking.component.ts
+++ src/app/client-side/read-client-bookings/read-client-booking/read-client-booking.component.ts
@@ -1,13 +1,29 @@
+import { Service } from './../../../models/service';
 import { BookingDetailsComponent } from './../../booking details/booking-details/booking-details.component';
-import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import {Component, OnInit} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface ClientBookingsTable {
+  id: number;
+  name: string;
+  service: string;
+  date: string;
+}
+
+const ELEMENT_DATA: ClientBookingsTable[] = [
+  {id: 1, name: 'Manesha Govender', service: 'Collection & Disposal of tires', date: '10/09/2021'},
+  {id: 2, name: 'Nerisha Ramjee ', service: 'Collection & Disposal of tires', date: '10/09/2021'},
+];
 
 @Component({
   selector: 'app-read-client-booking',
   templateUrl: './read-client-booking.component.html',
-  styleUrls: ['./read-client-booking.component.scss']
+  styleUrls: ['./read-client-booking.component.scss'],
 })
 export class ReadClientBookingComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'service', 'date', 'view'];
+  dataSource = ELEMENT_DATA;
   
   constructor(public dialog: MatDialog) { }
 
@@ -20,6 +36,6 @@ export class ReadClientBookingComponent implements OnInit {
       BookingDetailsComponent,
       dialogConfig
     );
-
+  
 }
 }
