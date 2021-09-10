5f94807869ffe0fa19f779c7ebdd1c59ea9b4bb7
Manesha99 <62016274+Manesha99@users.noreply.github.com>
2021-09-10 16:38:39 +0200

    filter

--- src/app/client-side/read-client-bookings/read-client-booking/read-client-booking.component.ts
+++ src/app/client-side/read-client-bookings/read-client-booking/read-client-booking.component.ts
@@ -24,6 +24,12 @@ const ELEMENT_DATA: ClientBookingsTable[] = [
 export class ReadClientBookingComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'service', 'date', 'view'];
   dataSource = ELEMENT_DATA;
+  dataSource1 = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource1.filter = filterValue.trim().toLowerCase();
+  }
   
   constructor(public dialog: MatDialog) { }
 
