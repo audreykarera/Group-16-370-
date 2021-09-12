b843fbac7b2ec238478b47b42f6887c04c1912d6
Manesha99 <62016274+Manesha99@users.noreply.github.com>
2021-09-12 18:33:32 +0200

    Update print-client-invoices.component.ts

--- src/app/systems/invoices/print-client-invoices/print-client-invoices/print-client-invoices.component.ts
+++ src/app/systems/invoices/print-client-invoices/print-client-invoices/print-client-invoices.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-print-client-invoices',
@@ -6,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print-client-invoices.component.scss']
 })
 export class PrintClientInvoicesComponent implements OnInit {
-  dialog: any;
+  invoice: { InvoiceID: number};
+  
 
-  constructor() { }
+  constructor( @Inject(MAT_DIALOG_DATA)
+  public data:any,public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    console.log(this.data);
+    this.refreshForm();
   }
+
+  refreshForm(){
+    this.invoice = {
+      InvoiceID: 0,
+    }
+  }
+
   Close(){
     this.dialog.closeAll();
   }
