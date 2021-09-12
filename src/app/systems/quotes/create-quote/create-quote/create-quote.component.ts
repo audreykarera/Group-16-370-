430f73f830da5e8c0f5b4a4b02ef66b822b3ff11
Manesha99 <62016274+Manesha99@users.noreply.github.com>
2021-09-12 08:39:26 +0200

    Client Side Buttons

--- src/app/systems/quotes/create-quote/create-quote/create-quote.component.ts
+++ src/app/systems/quotes/create-quote/create-quote/create-quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-quote',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateQuoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+  Close(){
+    this.dialog.closeAll();
   }
 
 }
